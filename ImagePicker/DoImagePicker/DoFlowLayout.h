a2a89daaf024ca2a28ce16843a4f51fff51833dc
Seungbo Cho <donobono@gmail.com>
2014-01-24 18:37:28 +0900

    working for custom flow layout

---
+++ ImagePicker/DoImagePicker/DoFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  DoFlowLayout.h
+//  ImagePicker
+//
+//  Created by Seungbo Cho on 2014. 1. 24..
+//  Copyright (c) 2014년 Seungbo Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ ImagePicker/DoImagePicker/DoFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  DoFlowLayout.m
+//  ImagePicker
+//
+//  Created by Seungbo Cho on 2014. 1. 24..
+//  Copyright (c) 2014년 Seungbo Cho. All rights reserved.
+//
+
+#import "DoFlowLayout.h"
+
+@implementation DoFlowLayout
+
+@end
